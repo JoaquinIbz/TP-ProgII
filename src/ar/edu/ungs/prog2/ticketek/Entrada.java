fb4f105c43251786a2ec82ded8153b3897bf76ab
joaquinLugoAcosta <joaquinlugo29@gmail.com>
2025-05-17 16:24:40 -0300

    se agregan funciones para comenzar a programar.

--- src/ar/edu/ungs/prog2/ticketek/Entrada.java
+++ src/ar/edu/ungs/prog2/ticketek/Entrada.java
@@ -1,4 +1,28 @@
 package ar.edu.ungs.prog2.ticketek;
 
 public class Entrada {
+
+    String codigoEntrada;
+    String nombreSede;
+    Sector sector;
+    int fila;
+    int asiento;
+    Espectaculo espectaculo;
+
+    public Entrada(String codigoEntrada){
+        this.codigoEntrada = codigoEntrada;
+    }
+
+    public void venderEntradaEstadio(String nombreEspectaculo, String nombreSede, String fecha, Sector sector){
+    }
+
+    public void venderEntrada(String nombreEspectaculo, String nombreSede, String fecha, Sector sector, int fila, int asiento){
+
+    }
+
+    public double precio(){
+
+    }
+
+
 }
--- src/ar/edu/ungs/prog2/ticketek/Espectaculo.java
+++ src/ar/edu/ungs/prog2/ticketek/Espectaculo.java
@@ -1,7 +1,28 @@
 package ar.edu.ungs.prog2.ticketek;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Espectaculo {
+    String nombre;
+    String codigo;
+    HashMap<String, LinkedList<Funcion>> funciones;
 
+    public Espectaculo(String nombre, String codigo){
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
 
+    public void agregarFuncion(Funcion funcion, String fecha){
+
+    }
+
+    public boolean verificarDisponibilidad(String fecha, String nombreSede){
+
+    }
+
+    public LinkedList<Funcion> obtenerFunciones(String fecha){
+
+    }
 
 }
