1be49a4678fc210a8a3f4231a2074407a8efe93d
joaquinLugoAcosta <joaquinlugo29@gmail.com>
2025-05-19 22:24:06 -0300

    se agregaron metodos

--- src/ar/edu/ungs/prog2/ticketek/Espectaculo.java
+++ src/ar/edu/ungs/prog2/ticketek/Espectaculo.java
@@ -5,24 +5,37 @@ import java.util.LinkedList;
 
 public class Espectaculo {
     String nombre;
-    String codigo;
-    HashMap<String, LinkedList<Funcion>> funciones;
+    //String codigo;
+    HashMap<String, LinkedList<Funcion>> funciones;  // fecha, funciones
 
-    public Espectaculo(String nombre, String codigo){
+    public Espectaculo(String nombre){
         this.nombre = nombre;
-        this.codigo = codigo;
+        this.funciones = new HashMap<>();
     }
 
     public void agregarFuncion(Funcion funcion, String fecha){
-
+        if(this.funciones.containsKey(fecha)){
+            if(this.funciones.get(fecha) != null){
+                throw new RuntimeException("La fecha ya esta ocupada");
+            }
+        }
+        LinkedList<Funcion> funciones = new LinkedList<>();
+        funciones.add(funcion);
+        this.funciones.put(fecha,funciones);
     }
 
-    public boolean verificarDisponibilidad(String fecha, String nombreSede){
-
+    public boolean verificarDisponibilidad(String fecha){
+        // verifica si la fecha esta disponible
+        if(this.funciones.containsKey(fecha)){
+            if(this.funciones.get(fecha) != null){
+                return false;
+            }
+        }
+        return true;
     }
 
     public LinkedList<Funcion> obtenerFunciones(String fecha){
-
+        return null;
     }
 
 }
