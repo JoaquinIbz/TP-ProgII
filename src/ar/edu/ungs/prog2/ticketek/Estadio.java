b4bb8a379cef6febcd6c2923ad343b07fcbdc997
joaquinLugoAcosta <joaquinlugo29@gmail.com>
2025-05-21 14:47:28 -0300

    agregue metodo venderEntrada, falta terminar

--- src/ar/edu/ungs/prog2/ticketek/Estadio.java
+++ src/ar/edu/ungs/prog2/ticketek/Estadio.java
@@ -1,10 +1,29 @@
 package ar.edu.ungs.prog2.ticketek;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Estadio extends Sede {
 
-    public Estadio(String nombre, String direccion, int capacidadMax) {//SAQUE EL PRECIO UNICO 
+    String sector;
+    HashMap<String,Entrada> entradasVendidas; // email, entrada
+
+    public Estadio(String nombre, String direccion, int capacidadMax) {
         super(nombre, direccion, capacidadMax);
-        this.sectores = new String[0];
-        this.sectores[0] = "CAMPO";
+        this.sector = "CAMPO";
+        this.capacidadActual = capacidadMax;
+        this.entradasVendidas = new HashMap<>();
     }
+
+    @Override
+    public Entrada venderEntrada(String email, String nombreSede, String nombreEspectaculo, String fecha){
+        Entrada entrada = new Entrada(nombreSede, nombreEspectaculo, fecha);
+        if(!this.entradasVendidas.containsKey(email)){
+            // TERMINAR, HACER CODIGO DE LA ENTRADA. ESO SE ENCARGA CADA SEDE.
+            return entrada;
+        }
+        return entrada;
+    }
+
+
 }
