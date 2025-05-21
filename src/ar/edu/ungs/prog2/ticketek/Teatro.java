c58e5728bfa048d5afcfb213f79c4355d24fb28d
joaquinLugoAcosta <joaquinlugo29@gmail.com>
2025-05-21 20:38:45 -0300

    sigo con metodo vender entrada

--- src/ar/edu/ungs/prog2/ticketek/Teatro.java
+++ src/ar/edu/ungs/prog2/ticketek/Teatro.java
@@ -1,44 +1,50 @@
 package ar.edu.ungs.prog2.ticketek;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Teatro extends Sede{
 
     String[] sectores; // {"Platea VIP", "Platea Común", "Platea Baja", "Platea Alta"} O {"Campo"}
     int asientosPorFila;
     int[] porcentajeAdicional;
-    HashMap<String,HashMap<Integer, Integer>> asientos; // [SECTOR] [FILA, ASIENTO]
+    HashMap<String,HashMap<Integer, Integer>> asientos;// [SECTOR] [FILA, ASIENTO]
 
-    public Teatro(String nombre, String direccion, int capacidadMax, int asientosPorFila, String[] sectores,
-    		int[] capacidad, int[] porcentajeAdicional) {
+    public Teatro(String nombre, String direccion, int capacidadMax, int asientosPorFila, String[] sectores, int[] capacidad, int[] porcentajeAdicional) {
     	super(nombre, direccion, capacidadMax);
         this.asientosPorFila = asientosPorFila;
         this.sectores = sectores;
         this.capacidad = capacidad;
         this.porcentajeAdicional = porcentajeAdicional;
-    	
-    	
+        this.asientos = new HashMap<>();
     }
 
-    public void reservarAsiento(String[] sector, int fila, int asiento) {
-        return;
-    }
+    public Entrada venderEntrada(String email, String nombreSede, String nombreEspectaculo, String fecha, String sector, int[] asientos) {
+        Entrada entrada = new Entrada(email,nombreSede,nombreEspectaculo,fecha,asientos,sector);
+        if(!this.entradasVendidas.containsKey(fecha)){
+            LinkedList<Entrada> e = new LinkedList<>();
+            e.add(entrada);
+            this.entradasVendidas.put(fecha,e);
+        }else{
+            this.entradasVendidas.get(fecha).add(entrada);
+        }
 
-    public void liberarAsiento(String[] sector, int fila, int asiento) {
-        return;
-    }
 
-    public boolean estaDisponible(String[] sector, int fila, int asiento) {
-        return false;
-    }
 
-    @Override
-    public Entrada venderEntrada(String email, String nombreSede, String nombreEspectaculo, String fecha) {
-        return null;
+        return entrada;
     }
 
     @Override
     public double calcularPrecio(double precioBase) {
         return 0;
     }
+
+    public void liberarAsiento(String sector, int[] asientos) {
+        return;
+    }
+
+    public boolean estaDisponible(String sector, int[] asientos) {
+        return false;
+    }
+
 }
