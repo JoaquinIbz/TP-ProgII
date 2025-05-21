f08f14cf41a13db1c37751b493ff9ccca301c8ff
joaquinLugoAcosta <joaquinlugo29@gmail.com>
2025-05-21 14:47:56 -0300

    falta terminar

--- src/ar/edu/ungs/prog2/ticketek/Entrada.java
+++ src/ar/edu/ungs/prog2/ticketek/Entrada.java
@@ -4,25 +4,42 @@ public class Entrada implements IEntrada {
 
     private String codigoEntrada;
     String nombreSede;
-    private Sector sector;
+    private String sector;
+    private String fecha;
     private int fila;
     private int asiento;
+    private String nombreEspectaculo;
     private Espectaculo espectaculo;
 
-    public Entrada(String codigoEntrada){
-        this.codigoEntrada = codigoEntrada;
+
+    public Entrada(String nombreSede, String nombreEspectaculo, String fecha){
+        this.nombreSede = nombreSede;
+        this.nombreEspectaculo = nombreEspectaculo;
+        this.fecha = fecha;
     }
 
     public double precio() {
         return 0;
     }
 
+    /**
+     * Si la entrada es para estadio, la ubicacion será "CAMPO".
+     * sino, será "{SECTOR} f:{NRO FILA} a:{NRO ASIENTO}"
+     *
+     * Por ejemplo:
+     *  - CAMPO
+     *  - Platea Común f:3 a:31
+     * @return
+     */
     public String ubicacion() {
         return "";
     }
 
-	public Espectaculo getEspectaculo() {
-		return espectaculo;
-	}
-    
+    public String toString(){
+        return "";
+    }
+
+    public Espectaculo getEspectaculo(){
+        return this.espectaculo;
+    }
 }
