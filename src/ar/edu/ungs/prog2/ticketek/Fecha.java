c25efaea823e85560de6a4cb77ca58fc4cc5637b
joaquinLugoAcosta <joaquinlugo29@gmail.com>
2025-05-23 16:42:02 -0300

    se creo clase fecha

--- src/ar/edu/ungs/prog2/ticketek/Fecha.java
+++ src/ar/edu/ungs/prog2/ticketek/Fecha.java
@@ -1,11 +1,16 @@
 package ar.edu.ungs.prog2.ticketek;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Fecha {
-	private LocalDate fecha;
+	private LocalDate fecha;									   //yy//mm/dd
+	private DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yy");
 	
 	public Fecha(String fecha) {
-		this.fecha = LocalDate.parse(fecha);
-		}
+		this.fecha = LocalDate.parse(fecha,formato);
+	}
+	public boolean esFutura(){
+		return this.fecha.isAfter(LocalDate.now());
+	}
 }
