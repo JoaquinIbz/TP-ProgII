765a17d7b67df128139d5e0fa9730d152991e49c
joaquinLugoAcosta <joaquinlugo29@gmail.com>
2025-05-24 16:37:46 -0300

    cambio de metodos

--- src/ar/edu/ungs/prog2/ticketek/Usuario.java
+++ src/ar/edu/ungs/prog2/ticketek/Usuario.java
@@ -3,6 +3,7 @@ package ar.edu.ungs.prog2.ticketek;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class Usuario {
 
@@ -10,89 +11,72 @@ public class Usuario {
     private String apellido;
     private String email;
     private String contrasenia;
-    private HashMap<String, LinkedList<Entrada>> entradasCompradas; // fecha, entrada
-    private HashMap<String, HashMap<String, Entrada>> mapaDeEntradas; //clave = fecha, valor= clave=codigo valor= Entrada
+    //private HashMap<String, LinkedList<Entrada>> entradasCompradas; // fecha, entrada
+    private HashMap<String, HashMap<Integer, Entrada>> entradasCompradas; //clave = fecha, valor= clave=codigo valor= Entrada
 
     public Usuario(String nombre, String apellido, String email, String contrasenia){
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
         this.contrasenia = contrasenia;
+        //this.entradasCompradas = new HashMap<>();
         this.entradasCompradas = new HashMap<>();
-        this.mapaDeEntradas = new HashMap<>();
     }
 
     public void comprarEntrada(Entrada entrada, String fecha){
-        if(!this.entradasCompradas.containsKey(fecha)){
-            LinkedList<Entrada> entradas = new LinkedList<>();
-            this.entradasCompradas.put(fecha, entradas);
-        }
-        LinkedList<Entrada> entradas = this.entradasCompradas.get(fecha);
-        entradas.add(entrada);
-        
      // Guardar en el mapa de códigos (mapaDeEntradas)
-        if (!this.mapaDeEntradas.containsKey(fecha)) {
-            this.mapaDeEntradas.put(fecha, new HashMap<>());
+        if (!this.entradasCompradas.containsKey(fecha)) {
+            this.entradasCompradas.put(fecha, new HashMap<>());
         }
-        
-        HashMap<String, Entrada> mapaCodigos = this.mapaDeEntradas.get(fecha);
-        mapaCodigos.put(entrada.getCodigo(), entrada); // O(1)
+        HashMap<Integer, Entrada> entradas = this.entradasCompradas.get(fecha);
+        entradas.put(entrada.getCodigo(), entrada); // O(1)
     }
 
     public LinkedList<Entrada> listarEntradasFuturas(){
-    	LinkedList<Entrada> listaEntradas = new LinkedList<>();
-        for(String f : entradasCompradas.keySet()){
-            Fecha fecha = new Fecha(f);
-        	if(fecha.esFutura()) {
-        		listaEntradas.addAll(entradasCompradas.get(f));
-        	}
+    	LinkedList<Entrada> entradasFuturas = new LinkedList<>();
+        for(Map.Entry<String,HashMap<Integer,Entrada>> entry : this.entradasCompradas.entrySet()){
+            Fecha fecha = new Fecha(entry.getKey());
+            if(fecha.esFutura()){
+                HashMap<Integer,Entrada> entradas = entry.getValue();
+                for(Entrada e : entradas.values()){
+
+                    entradasFuturas.add(e);
+                }
+            }
         }
-        return listaEntradas;
+        return entradasFuturas;
     }
 
     public LinkedList<Entrada> listarEntradasPasadas(){
-        LinkedList<Entrada> listaEntradas = new LinkedList<>();
-        for(String f : entradasCompradas.keySet()) {
-        	Fecha fecha = new Fecha(f);
-        	if(fecha.esPasada()) {
-        		listaEntradas.addAll(entradasCompradas.get(f));
-        	}
+        LinkedList<Entrada> entradasPasadas = new LinkedList<>();
+        for(Map.Entry<String,HashMap<Integer,Entrada>> entry : this.entradasCompradas.entrySet()){
+            Fecha fecha = new Fecha(entry.getKey());
+            if(fecha.esPasada()){
+                HashMap<Integer,Entrada> entradas = entry.getValue();
+                for(Entrada e : entradas.values()){
+                    entradasPasadas.add(e);
+                }
+            }
         }
-        return listaEntradas;
+        return entradasPasadas;
     }
 
     public LinkedList<Entrada> listarTotalEntradas(){
         LinkedList<Entrada> total = new LinkedList<>();
-        for(LinkedList<Entrada> entrada : entradasCompradas.values()) {
-        	total.addAll(entrada);
-        }
+        total.addAll(listarEntradasPasadas());
+        total.addAll(listarEntradasFuturas());
         return total;
     }
     
     public boolean anularEntrada(Entrada entrada, Sede sede) {
     	String fecha = entrada.getFecha();
-    	String codigo = entrada.getCodigo();
-    	HashMap<String, Entrada> mapa = mapaDeEntradas.get(fecha);
+    	int codigo = entrada.getCodigo();
+    	HashMap<Integer, Entrada> mapa = this.entradasCompradas.get(fecha);
         if(mapa == null || !mapa.containsKey(codigo)) {
         	throw new RuntimeException("Entrada no encontrada");
         }
         mapa.remove(codigo);
-        
-     // Liberar asiento (O(1))
-        
-        
-       // sede.liberarAsiento(entrada.getSector(), entrada.getButaca().getFila(), entrada.getButaca().getAsiento());
-        if(sede instanceof Estadio) {
-        	((Estadio) sede).anularEntrada();
-        }
-        if(sede instanceof MiniEstadio) {
-        	((MiniEstadio) sede).liberarAsiento(entrada.getSector(), entrada.getButaca().getFila(), entrada.getButaca().getAsiento());
-        }
-        if(sede instanceof Teatro) {
-        	((Teatro) sede).liberarAsiento(entrada.getSector(), entrada.getButaca().getFila(), entrada.getButaca().getAsiento());
-        }
-        	
-
+        sede.anularEntrada(entrada.getSector(), entrada.getButaca().getFila(), entrada.getButaca().getAsiento());
         return true;
     }
 
